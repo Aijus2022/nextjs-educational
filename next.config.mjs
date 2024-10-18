ed0f338ca4dcacc9f5dadb70f5a64cc30829374e
Aijamal Jusupova <109210016+Aijus2022@users.noreply.github.com>
2024-10-18 05:37:27 +0100

    Update next.config.js for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
-export default nextConfig;
 module.exports = {
-    basePath: '/https://github.com/Aijus2022/nextjs-educational.git',
-    assetPrefix: '/https://github.com/Aijus2022/nextjs-educational.git/',
-  }
-  
+  basePath: '/nextjs-educational',
+  assetPrefix: '/nextjs-educational/',
+}
+
