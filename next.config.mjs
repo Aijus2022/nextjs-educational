d8c325d2a310d69b7af20b1818e78839b72ca5e4
Aijamal Jusupova <109210016+Aijus2022@users.noreply.github.com>
2024-10-18 05:25:42 +0100

    Add GitHub Actions workflow for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,8 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+    basePath: '/https://github.com/Aijus2022/nextjs-educational.git',
+    assetPrefix: '/https://github.com/Aijus2022/nextjs-educational.git/',
+  }
+  
